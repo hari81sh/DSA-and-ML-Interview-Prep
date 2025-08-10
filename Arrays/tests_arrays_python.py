d6a16583e682cad065799d2d173b54668a0f0c69
harizh917 <medikondaharish.hc@gmail.com>
2025-08-10 17:16:49 +0530

    renamed files

--- Arrays/tests_arrays_python.py
+++ Arrays/tests_arrays_python.py
@@ -1,18 +1,18 @@
 import unittest
-import python_practice
+import python_arrays_practice
 print(unittest.__file__)
 
 class test_arrays_unit(unittest.TestCase):
 
     
     def test_words_containing_one(self):  # Renamed to start with 'test_'
-        words = python_practice.Words_Containing_Character
+        words = python_arrays_practice.Words_Containing_Character
         self.assertEqual(words.findWordsContaining(words=["leet","code"], x='e'), [0,1])
         print("Test 'test_words_containing_one' passed successfully!")
 
 
     def test_build_array_from_permutation(self):
-        arr = python_practice.Build_Array_from_permutation
+        arr = python_arrays_practice.Build_Array_from_permutation
         self.assertEqual(arr.build_array([0,2,1,5,3,4]),[0, 1, 2, 4, 5, 3])
         print("Test 'test_build_array_from_permutation' passed successfully!")
 
